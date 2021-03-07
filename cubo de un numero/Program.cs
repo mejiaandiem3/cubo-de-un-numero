1484a7420de4a352d761197deb6940ad9cc58c19
Andrea Evelyn Mejía Rubio <andiemartin33@gmail.com>
2021-03-07 01:45:54 -0600

    Agregar archivos de proyecto.

---
+++ cubo de un numero/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cubo_de_un_numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double numero;
+            double cubo;
+
+
+            Console.WriteLine("El cubo de un numero. ");
+            Console.WriteLine(" ");
+            Console.WriteLine("Escribe el numero: ");
+            numero = Convert.ToDouble(Console.ReadLine());
+
+
+            cubo  = numero * numero * numero;
+
+
+            Console.WriteLine("Este es el cubo del numero: " + cubo);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
